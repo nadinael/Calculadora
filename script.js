0fe5e78866df8bd405e96c1c9556f2ebd7a2a701
Nadinael Silva <nadinael.silva@gmail.com>
2023-03-27 03:18:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function insert(num) {
+  var numero = document.getElementById('resultado').innerHTML;
+  console.log("log: "+numero);
+if(numero === "Nada para calcular."){
+  //console.log("cond ");
+  //alert("NADA");
+  numero = document.getElementById('resultado').innerHTML = "";
+}
+  
+  document.getElementById('resultado').innerHTML = numero + num;
+}
+function limpar() {
+  document.getElementById('resultado').innerHTML = "";
+}
+
+function setar() {
+  var res = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = res.substring(0,res.length -1);
+//console.log(""+res.length);
+}
+
+function calcular(){
+  var calculo = document.getElementById('resultado').innerHTML;
+  if(calculo){
+    document.getElementById('resultado').innerHTML = eval(calculo);
+  }else {
+    document.getElementById('resultado').innerHTML = "Nada para calcular.";
+  }
+}

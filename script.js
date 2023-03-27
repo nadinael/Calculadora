ff3b95b3122b5f53230c6da0e2d7bfacc28451fb
Nadinael Silva <nadinael.silva@gmail.com>
2023-03-27 03:22:55 +0000

    fixes.

--- script.js
+++ script.js
@@ -12,13 +12,11 @@ if(numero === "Nada para calcular."){
 function limpar() {
   document.getElementById('resultado').innerHTML = "";
 }
-
 function setar() {
   var res = document.getElementById('resultado').innerHTML;
   document.getElementById('resultado').innerHTML = res.substring(0,res.length -1);
 //console.log(""+res.length);
 }
-
 function calcular(){
   var calculo = document.getElementById('resultado').innerHTML;
   if(calculo){
